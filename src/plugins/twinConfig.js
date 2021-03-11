7a1416a8cb1c658c83130cb497a75379ed8dde23
momo3207778@126.com <momo3207778@126.com>
2021-03-11 16:19:01 +0800

    提交

--- src/plugins/twinConfig.js
+++ src/plugins/twinConfig.js
@@ -45,7 +45,7 @@ export default function () {
         icon: "icon-user", // icon 路由图标 type=iconfont是字体图标的class、type=image是图片地址
         type: "iconfont", // type 路由图标类型  iconfont => 字体图标、image => 图片 
         name: "个人中心",
-        onclick: function() {
+        onclick: function () {
           twinLayout.$router.push({
             name: "PersonalCenter"
           });
@@ -56,7 +56,7 @@ export default function () {
         type: "iconfont",
         name: "退出",
         topLine: true,
-        onclick: function() {
+        onclick: function () {
           store.commit("emptyUserInfo");
           // 销毁双生布局
           destroyTwin();
@@ -109,119 +109,130 @@ export default function () {
           "iconType": "iconfont",
           "permissions": ["view", "add", "modify", "delete"]
         }
-      },{
-          "path": "/articleManage",
-          "meta": {
-              "title": "文章管理",
-              "icon": "icon-wenzhang-copy"
-          },
-          "children": [{
-              "hidden": false,
-              "path": "articleManage/articleList/index",
-              "name": "ArticleList",
-              "type": "component",
-              "meta": {
-                  "title": "文章列表",
-                  "icon": "icon-wenzhang-copy",
-                  "iconType": "iconfont",
-                  "permissions": ["view", "add", "modify", "delete"]
-              }
-          }, {
-              "hidden": false,
-              "path": "articleManage/classifyList/index",
-              "name": "ClassifyList",
-              "type": "component",
-              "meta": {
-                  "title": "文章分类列表",
-                  "icon": "icon-fenlei1",
-                  "iconType": "iconfont",
-                  "permissions": ["view", "add", "modify", "delete"]
-              }
-          }, {
-              "hidden": true,
-              "path": "articleManage/articleList/addModify",
-              "name": "ArticleAddModify",
-              "type": "component",
-              "meta": {
-                  "title": "添加文章",
-                  "icon": "icon-rizhi",
-                  "iconType": "iconfont",
-                  "permissions": ["view", "add", "modify", "delete"]
-              }
-            },
-          ]
+      }, {
+        "path": "/articleManage",
+        "meta": {
+          "title": "文章管理",
+          "icon": "icon-wenzhang-copy"
         },
-        {
-          "path": "/iframeTest",
-          "redirect": "noRedirect",
-          "meta": {
-            "title": "iframe示例",
-            "icon": "icon-rizhi"
-          },
-          "children": [
-            {
-              "hidden": false,
-              "path": "webpage2.html",
-              "name": "IframeDemo1",
-              "type": "iframe",
-              "meta": {
-                "title": "iframe示例1",
-                "icon": "icon-fuwuqi",
-                "iconType": "iconfont",
-                "permissions": [
-                  "view",
-                  "add",
-                  "modify",
-                  "delete"
-                ]
-              }
-            },
-            {
-              "hidden": false,
-              "path": "webpage.html",
-              "name": "IframeTest2",
-              "type": "iframe",
-              "meta": {
-                "title": "iframe示例2",
-                "icon": "icon-drxx05",
-                "iconType": "iconfont",
-                "permissions": [
-                  "view",
-                  "add",
-                  "modify",
-                  "delete"
-                ]
-              }
-            }
-          ]
-        },
-        {
-          "hidden": true,
-          "path": "common/personalCenter",
-          "name": "PersonalCenter",
+        "children": [{
+          "hidden": false,
+          "path": "articleManage/articleList/index",
+          "name": "ArticleList",
           "type": "component",
           "meta": {
-            "title": "个人中心",
-            "icon": "icon-yonghu",
+            "title": "文章列表",
+            "icon": "icon-wenzhang-copy",
             "iconType": "iconfont",
             "permissions": ["view", "add", "modify", "delete"]
           }
-        }]);
+        }, {
+          "hidden": false,
+          "path": "articleManage/classifyList/index",
+          "name": "ClassifyList",
+          "type": "component",
+          "meta": {
+            "title": "文章分类列表",
+            "icon": "icon-fenlei1",
+            "iconType": "iconfont",
+            "permissions": ["view", "add", "modify", "delete"]
+          }
+        }, {
+          "hidden": true,
+          "path": "articleManage/articleList/addModify",
+          "name": "ArticleAddModify",
+          "type": "component",
+          "meta": {
+            "title": "添加文章",
+            "icon": "icon-rizhi",
+            "iconType": "iconfont",
+            "permissions": ["view", "add", "modify", "delete"]
+          }
+        },
+        ]
+      },
+      {
+        "path": "/iframeTest",
+        "redirect": "noRedirect",
+        "meta": {
+          "title": "iframe示例",
+          "icon": "icon-rizhi"
+        },
+        "children": [
+          {
+            "hidden": false,
+            "path": "webpage2.html",
+            "name": "IframeDemo1",
+            "type": "iframe",
+            "meta": {
+              "title": "iframe示例1",
+              "icon": "icon-fuwuqi",
+              "iconType": "iconfont",
+              "permissions": [
+                "view",
+                "add",
+                "modify",
+                "delete"
+              ]
+            }
+          },
+          {
+            "hidden": false,
+            "path": "webpage.html",
+            "name": "IframeTest2",
+            "type": "iframe",
+            "meta": {
+              "title": "iframe示例2",
+              "icon": "icon-drxx05",
+              "iconType": "iconfont",
+              "permissions": [
+                "view",
+                "add",
+                "modify",
+                "delete"
+              ]
+            }
+          }
+        ]
+      },
+      {
+        "hidden": true,
+        "path": "common/personalCenter",
+        "name": "PersonalCenter",
+        "type": "component",
+        "meta": {
+          "title": "个人中心",
+          "icon": "icon-yonghu",
+          "iconType": "iconfont",
+          "permissions": ["view", "add", "modify", "delete"]
+        }
+      }]);
       // getRouterList().then(res => {
       //   callbcak(res.data);
       // });
     },
     // 消息通知点击事件
-    noticeClick: () => {
-      MessageBox.alert("请自定义消息弹窗", "提示", {
-        confirmButtonText: "确定",
-        dangerouslyUseHTMLString: true,
-      });
+    noticeClick: (options, callback) => {
+      callback(`
+        <div style="padding: 15px;">
+          <div style="padding: 5px 0;">消息展开</div>
+          <div style="padding: 5px 0;">消息展开</div>
+        </div>
+      `);
     },
+    // 消息通知hover事件
+    // noticeHover: (options, callback) => {
+    //   callback(`
+    //     <div style="padding: 15px;">
+    //       <div style="padding: 5px 0;">消息展开</div>
+    //       <div style="padding: 5px 0;">消息展开</div>
+    //     </div>
+    //   `);
+    // },
     // 错误信息
     onError(err) {
       window.console.error(err.message);
-      if (err.type == "danger") { 
+      if (err.type == "danger") {
         Message({
           offset: 60,
           showClose: true,
@@ -238,21 +249,21 @@ export default function () {
     /* element-ui的主题覆盖修改 */
     return `.el-button--primary,
         .el-tag--dark {
-            background-color: rgba(${data.themeColor }, 1) !important;
-            border-color: rgba(${data.themeColor }, 1) !important;
+            background-color: rgba(${data.themeColor}, 1) !important;
+            border-color: rgba(${data.themeColor}, 1) !important;
         }
         .el-tabs__active-bar,
         .el-pagination.is-background .el-pager li:not(.disabled).active {
-            background-color: rgba(${data.themeColor }, 1) !important;
+            background-color: rgba(${data.themeColor}, 1) !important;
         }
         .theme_border_color,
         .el-pagination.is-background .el-pager li:not(.disabled).active {
-            border-color: rgba(${data.themeColor }, 1) !important;
+            border-color: rgba(${data.themeColor}, 1) !important;
         }
         .el-tag {
-            background-color: rgba(${data.themeColor }, 0.1) !important;
-            border-color: rgba(${data.themeColor }, 0.1) !important;
-            color: rgba(${data.themeColor }, 1) !important;
+            background-color: rgba(${data.themeColor}, 0.1) !important;
+            border-color: rgba(${data.themeColor}, 0.1) !important;
+            color: rgba(${data.themeColor}, 1) !important;
         }
         .theme_color,
         .el-loading-text,
@@ -261,15 +272,15 @@ export default function () {
         .el-tabs__item:hover,
         .el-pagination.is-background .el-pager li:not(.disabled):hover,
         .el-radio-button__inner:hover {
-            color: rgba(${data.themeColor }, 1) !important;
+            color: rgba(${data.themeColor}, 1) !important;
         }
 
         .github-corner .github-color {
-            fill: rgba(${data.themeColor }, 1) !important;
+            fill: rgba(${data.themeColor}, 1) !important;
         }
 
         .el-loading-spinner .path {
-            stroke: rgba(${data.themeColor }, 1) !important;
+            stroke: rgba(${data.themeColor}, 1) !important;
         }`
   }
   // 窗口关闭
@@ -307,7 +318,7 @@ export default function () {
             },
           }
         });
-        if (page.default.twinShow) { 
+        if (page.default.twinShow) {
           event.twinShow = page.default.twinShow.bind(pageObj);
         }
         if (page.default.twinHide) {
@@ -331,6 +342,6 @@ export default function () {
   }, 5000);
   // 开始创建双生布局
   twinLayout.create(() => {
-    
+
   });
 }
